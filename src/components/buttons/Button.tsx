1977a49a54115cecbfa7e7154eb7a41c6e55fd81
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 04:54:29 -0500

    feat: button

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    text: string;
+}
+
+export const Button = ({
+    className,
+    text,
+    ...props
+}: ButtonProps) => {
+  return (
+    <button
+        className={twMerge("w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base")}
+        {...props}
+    >
+        {text}
+    </button>
+  )
+}
