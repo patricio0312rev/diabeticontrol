02fc78faa9f30ba4154174cd53f5b7aa395b1d91
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 14:41:35 -0500

    feat: disabled status for button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -10,7 +10,7 @@ export const Button = ({ className, text, ...props }: ButtonProps) => {
   return (
     <button
       className={twMerge(
-        "w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base",
+        "w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base disabled:bg-theme-secondary-300 disabled:text-theme-secondary-500 disabled:cursor-not-allowed",
         className
       )}
       {...props}
