f853406ed84be92a0534ed0cf900e2e0287624c0
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 05:45:57 -0500

    fix: button custom class names

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { twMerge } from 'tailwind-merge';
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    className?: string;
-    text: string;
+  className?: string;
+  text: string;
 }
 
-export const Button = ({
-    className,
-    text,
-    ...props
-}: ButtonProps) => {
+export const Button = ({ className, text, ...props }: ButtonProps) => {
   return (
     <button
-        className={twMerge("w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base")}
-        {...props}
+      className={twMerge(
+        "w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base",
+        className
+      )}
+      {...props}
     >
-        {text}
+      {text}
     </button>
-  )
-}
+  );
+};
