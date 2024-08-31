02294726d788a16c7431dbbf7182dad3edb0a6a8
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 04:55:24 -0500

    feat: animated container

---
+++ src/components/layout/AnimatedContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge';
+import { Navbar } from './Navbar';
+
+export const AnimatedContainer = ({
+  children,
+  withNavbar = false,
+  className
+}: {
+  children: React.ReactNode;
+  withNavbar?: boolean;
+  className?: string;
+}) => {
+  return (
+    <div className={twMerge('min-h-screen flex relative overflow-hidden', className)}>
+      {withNavbar && <Navbar />}
+      <div className="absolute inset-0 bg-radial-gradient bg-200 animate-gradient-bg" />
+      {children}
+    </div>
+  )
+}
