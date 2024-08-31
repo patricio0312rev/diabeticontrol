c8bc3dcec92586a02b69addf398d7dc1a50d4f2b
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 04:56:46 -0500

    feat: text link

---
+++ src/components/buttons/TextLink.tsx
@@ -0,0 +1,20 @@
+import Link, { LinkProps } from 'next/link';
+import React from 'react'
+import { twMerge } from 'tailwind-merge';
+
+interface TextLinkProps extends LinkProps {
+    text: string;
+    className?: string;
+}
+
+export const TextLink = ({
+    text,
+    className,
+    ...props
+}: TextLinkProps) => {
+    return (
+        <Link className={twMerge('font-medium text-theme-primary-600 hover:text-theme-primary-500', className)} {...props}>
+            {text}
+        </Link>
+    )
+}
--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,1 +1,2 @@
 export { Button } from './Button';
+export { TextLink } from './TextLink';
