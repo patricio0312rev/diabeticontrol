19d4c4d25da933a3ed98080343d18d19626a890f
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 20:42:45 -0500

    chore: udpate seeders with dummy data

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -23,6 +23,125 @@ async function main() {
       email: "admin@example.com",
     },
   });
+
+  await prisma.patient.upsert({
+    where: { id: "1" },
+    update: {},
+    create: {
+      id: "1",
+      firstName: "Juan Patricio",
+      lastName: "Marroquin Gavelan",
+      birthDate: new Date("1999-12-03"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "1" },
+    update: {},
+    create: {
+      id: "1",
+      patientId: "1",
+      type: "glucose",
+      value: "70",
+      createdAt: new Date("2024-08-01"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "2" },
+    update: {},
+    create: {
+      id: "2",
+      patientId: "1",
+      type: "glucose",
+      value: "80",
+      createdAt: new Date("2024-07-02"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "3" },
+    update: {},
+    create: {
+      id: "3",
+      patientId: "1",
+      type: "glucose",
+      value: "60",
+      createdAt: new Date("2024-06-03"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "4" },
+    update: {},
+    create: {
+      id: "4",
+      patientId: "1",
+      type: "glucose",
+      value: "70",
+      createdAt: new Date("2024-05-04"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "5" },
+    update: {},
+    create: {
+      id: "5",
+      patientId: "1",
+      type: "homoglobin-a1c",
+      value: "5.7",
+      createdAt: new Date("2024-04-05"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "6" },
+    update: {},
+    create: {
+      id: "6",
+      patientId: "1",
+      type: "homoglobin-a1c",
+      value: "5.8",
+      createdAt: new Date("2024-05-06"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "7" },
+    update: {},
+    create: {
+      id: "7",
+      patientId: "1",
+      type: "homoglobin-a1c",
+      value: "5.9",
+      createdAt: new Date("2024-06-07"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "8" },
+    update: {},
+    create: {
+      id: "8",
+      patientId: "1",
+      type: "homoglobin-a1c",
+      value: "7.0",
+      createdAt: new Date("2024-07-08"),
+    },
+  });
+
+  await prisma.record.upsert({
+    where: { id: "9" },
+    update: {},
+    create: {
+      id: "9",
+      patientId: "1",
+      type: "homoglobin-a1c",
+      value: "5.1",
+      createdAt: new Date("2024-08-09"),
+    },
+  });
 }
 
 main()
