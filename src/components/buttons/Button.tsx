cc5ce507a5c3652e81bcc89c2b7829c9160c096c
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-09-01 03:39:47 -0500

    refactor: button secondary

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
+import cn from "classnames";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   text: string;
+  variant?: "primary" | "secondary";
 }
 
-export const Button = ({ className, text, ...props }: ButtonProps) => {
+export const Button = ({
+  className,
+  text,
+  variant = "primary",
+  ...props
+}: ButtonProps) => {
   return (
     <button
       className={twMerge(
-        "w-full bg-theme-primary-600 hover:bg-theme-primary-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base disabled:bg-theme-secondary-300 disabled:text-theme-secondary-500 disabled:cursor-not-allowed",
+        "w-full font-bold py-2 px-4 rounded-md transition duration-300 text-sm sm:text-base disabled:bg-theme-secondary-300 disabled:text-theme-secondary-500 disabled:cursor-not-allowed",
+        cn({
+          "bg-theme-secondary-100 text-theme-primary-600 hover:bg-theme-secondary-200 border border-theme-primary-200 shadow-sm":
+            variant === "secondary",
+          "bg-theme-primary-600 hover:bg-theme-primary-700 text-white":
+            variant === "primary",
+        }),
         className
       )}
       {...props}
