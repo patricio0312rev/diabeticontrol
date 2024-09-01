07fb234ed4e39ac98bf5219ad4b5d182e042070b
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 19:42:15 -0500

    feat: logout api route

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    return NextResponse.json(
+      { success: true, message: "Cesión cerrada satisfactoriamente." },
+      {
+        status: 200,
+        headers: {
+          "Set-Cookie": "token=; HttpOnly; Path=/; Max-Age=0",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error during logout:", error);
+    return NextResponse.json(
+      { success: false, message: "Error interno." },
+      { status: 500 }
+    );
+  }
+}
