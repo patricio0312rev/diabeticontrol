b35e7ca1dd2ff5ed7ab23350aedf287255892c68
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 23:19:35 -0500

    chore: update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,8 @@
 export const NUMERIC_REGEX = /^[0-9]{0,8}$/;
+export const GLUCOSE = "glucose";
+export const HOMOGLOBIN_A1C = "homoglobin-a1c";
+export enum PatientTabs {
+  GLUCOSE = "glucose",
+  HOMOGLOBIN_A1C = "homoglobin-a1c",
+  PATIENT = "patient",
+}
