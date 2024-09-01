119885a1a24cdb0b77d60736983cae0c03d77cb4
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 23:20:05 -0500

    feat: dynamic patient info tab

--- src/components/pages/home/PatientInfoTab.tsx
+++ src/components/pages/home/PatientInfoTab.tsx
@@ -1,21 +1,21 @@
 import { InfoSection } from "@/components/common";
+import { Patient } from "@prisma/client";
 import React from "react";
 
-export const PatientInfoTab = () => {
+export const PatientInfoTab = ({ patient }: { patient: Patient }) => {
+  const birthDate = patient.birthDate
+    ? `${new Date().getFullYear() - new Date(patient.birthDate).getFullYear()}`
+    : "N/A";
+  const createdAt = new Date(patient.createdAt).toLocaleDateString();
   return (
     <div className="mb-6">
       <h2>Información del Paciente</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <InfoSection label="Nombre" value={"Patricio"} />
-        <InfoSection label="Apellido" value={"Marroquin"} />
-        <InfoSection label="DNI" value={"75603331"} />
-        <InfoSection label="Fecha de Registro" value={"03/12/1999"} />
-        <InfoSection label="Edad" value={"24"} />
-        {/* <InfoSection
-          label="Fecha de Registro"
-          value={patient.registrationDate.toLocaleDateString()}
-        />
-        <InfoSection label="Edad" value={patient.age.toString()} /> */}
+        <InfoSection label="Nombre" value={patient.firstName} />
+        <InfoSection label="Apellido" value={patient.lastName} />
+        <InfoSection label="DNI" value={patient.id} />
+        <InfoSection label="Fecha de Registro" value={createdAt} />
+        <InfoSection label="Edad" value={birthDate} />
       </div>
     </div>
   );
