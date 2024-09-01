8b39e9488205d75616300c781f2a786dc3a88fe6
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-31 23:21:02 -0500

    feat: dashboard functionality

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,92 +4,73 @@ import { Container, TabNavigation } from "@/components/common";
 import { TextInput } from "@/components/inputs";
 import { SelectInput, TabOption } from "@/components/inputs/SelectInput";
 import { AnimatedContainer, Navbar } from "@/components/layout";
-import { GlucoseTab, PatientInfoTab } from "@/components/pages/home";
-import { NUMERIC_REGEX } from "@/utils/constants";
+import { RecordTab, PatientInfoTab } from "@/components/pages/home";
+import {
+  GLUCOSE,
+  HOMOGLOBIN_A1C,
+  NUMERIC_REGEX,
+  PatientTabs,
+} from "@/utils/constants";
 import React, { useState, useEffect } from "react";
+import axiosInstance from "@/utils/axios";
+import { useQuery } from "@tanstack/react-query";
+import { Patient, Record } from "@prisma/client";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { toast } from "react-toastify";
 
-type DataPoint = {
-  date: Date;
-  value: number;
-};
+interface PatientResponse extends Patient {
+  records: Record[];
+}
 
-type TabData = {
-  glucose: DataPoint[];
-  "homoglobin-a1c": DataPoint[];
-  patient: DataPoint[];
-};
-
-type Patient = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  registrationDate: Date;
-  age: number;
+const usePatient = (id: string) => {
+  return useQuery<PatientResponse, Error>({
+    queryKey: ["patient", id],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get(`/patients/${id}`);
+      return data.patient;
+    },
+    enabled: !!id && id.length === 8,
+    retry: 0,
+  });
 };
 
 export default function HomePage() {
   const [activeTab, setActiveTab] = useState<string>("patient");
-  const [inputValue, setInputValue] = useState<string>("");
-  const [data, setData] = useState<TabData>({
-    glucose: [],
-    "homoglobin-a1c": [],
-    patient: [],
-  });
-  const [todayEntered, setTodayEntered] = useState<boolean>(false);
   const [patientId, setPatientId] = useState<string>("");
-  const [patient, setPatient] = useState<Patient | null>(null);
+
+  const { data: patient, isSuccess, error, isError } = usePatient(patientId);
 
   useEffect(() => {
-    const today = new Date().toISOString().split("T")[0];
-    setTodayEntered(
-      data[activeTab as "glucose" | "homoglobin-a1c"].some(
-        (point) => point.date.toISOString().split("T")[0] === today
-      )
-    );
-  }, [data, activeTab]);
+    if (isSuccess) {
+      toast.success("Paciente cargado correctamente.");
+    } else if (isError && error) {
+      toast.error(`Error al buscar paciente: ${error.message}`);
+    }
+  }, [isSuccess, isError, error]);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const value = parseFloat(inputValue);
-    if (isNaN(value)) return;
+  const getPatientValidationSchema = Yup.object().shape({
+    patientId: Yup.string()
+      .matches(NUMERIC_REGEX, "El DNI debe ser numérico.")
+      .length(8, "El DNI debe tener 8 dígitos.")
+      .required("El DNI es obligatorio."),
+  });
 
-    const today = new Date();
-    const newData = [
-      ...data[activeTab as "glucose" | "homoglobin-a1c"],
-      { date: today, value },
-    ];
-    setData((prevData) => ({ ...prevData, [activeTab]: newData }));
-    setInputValue("");
-    setTodayEntered(true);
-  };
-
-  const handleEdit = () => {
-    setTodayEntered(false);
-  };
-
-  const handlePatientSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Mock patient data
-    const mockPatient: Patient = {
-      id: patientId,
-      firstName: "John",
-      lastName: "Doe",
-      registrationDate: new Date("2023-01-01"),
-      age: 35,
-    };
-    setPatient(mockPatient);
-    setActiveTab("patient");
-  };
-
-  const handlePatientIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    NUMERIC_REGEX.test(e.target.value) && setPatientId(e.target.value);
-  };
+  const formik = useFormik<{ patientId: string }>({
+    initialValues: {
+      patientId: "",
+    },
+    validationSchema: getPatientValidationSchema,
+    onSubmit: (values) => {
+      setPatientId(values.patientId);
+    },
+  });
 
   const options: TabOption[] = [
-    { value: "patient", label: "Información del Paciente" },
-    { value: "glucose", label: "Glucosa", disabled: !patient },
+    { value: PatientTabs.PATIENT, label: "Información del Paciente" },
+    { value: PatientTabs.GLUCOSE, label: "Glucosa", disabled: !patient },
     {
-      value: "homoglobin-a1c",
+      value: PatientTabs.HOMOGLOBIN_A1C,
       label: "Homoglobina Glicosilada",
       disabled: !patient,
     },
@@ -103,24 +84,26 @@ export default function HomePage() {
         <Container className="max-w-4xl my-8">
           <div className="mb-8">
             <form
-              onSubmit={handlePatientSearch}
+              onSubmit={formik.handleSubmit}
               className="flex sm:gap-4 sm:flex-row flex-col gap-2 w-full"
             >
               <TextInput
                 type="text"
-                value={patientId}
-                onChange={handlePatientIdChange}
+                name="patientId"
+                onChange={formik.handleChange}
                 placeholder="Ingresa el DNI del paciente"
                 label="Buscar paciente"
-                className="flex-grow "
+                className="flex-grow"
                 wrapperClassName="w-full"
+                value={formik.values.patientId}
+                onBlur={formik.handleBlur}
               />
 
               <Button
                 text="Buscar"
                 type="submit"
                 className="w-auto"
-                disabled={patientId.length < 7}
+                disabled={formik.values.patientId.length < 8 || !formik.isValid}
               />
             </form>
           </div>
@@ -144,10 +127,26 @@ export default function HomePage() {
             </div>
           </div>
 
-          {activeTab === "patient" && patient && <PatientInfoTab />}
+          {activeTab === PatientTabs.PATIENT && patient && (
+            <PatientInfoTab patient={patient} />
+          )}
 
-          {activeTab === "glucose" && patient && (
-            <GlucoseTab handleSubmit={handleSubmit} todayEntered={false} />
+          {activeTab === PatientTabs.GLUCOSE && patient && (
+            <RecordTab
+              handleSubmit={() => {}}
+              data={patient.records.filter((r) => r.type === GLUCOSE)}
+              todayEntered={false}
+              type={PatientTabs.GLUCOSE}
+            />
+          )}
+
+          {activeTab === PatientTabs.HOMOGLOBIN_A1C && patient && (
+            <RecordTab
+              handleSubmit={() => {}}
+              data={patient.records.filter((r) => r.type === HOMOGLOBIN_A1C)}
+              todayEntered={false}
+              type={PatientTabs.HOMOGLOBIN_A1C}
+            />
           )}
         </Container>
       </div>
